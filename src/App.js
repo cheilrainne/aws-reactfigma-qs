3827b677baa1f9ee673346d71c963d4911d7a02e
cheilrainne <cheilalsb@gmail.com>
2022-06-17 14:21:20 +0400

    Update GraphQL API backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import React, { useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { DataStore, Predicates } from "aws-amplify";
+import { Registration } from './models';
+
+//Use next two lines only if syncing with the cloud
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
+
+
+async function onQuery() {
+  const posts = await DataStore.query(Registration);
+  console.log(posts);
+}
+
+
 function App() {
+
+  useEffect(() => {
+    const subscription = DataStore.observe(Registration).subscribe((msg) => {
+      console.log(msg.model, msg.opType, msg.element);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
+        <div>
+          <input type="button" value="QUERY" onClick={onQuery} />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Amplified
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
