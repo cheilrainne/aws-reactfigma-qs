efe0d62b815902aea7476ab4d9d33afe04fe050b
cheilrainne <cheilalsb@gmail.com>
2022-06-20 11:17:51 +0400

    Add dependency for @aws-amplify/ui-react

---
+++ src/ui-components/TestStandardCard.jsx
@@ -0,0 +1,37 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import StandardCard from "./StandardCard";
+import { Flex } from "@aws-amplify/ui-react";
+export default function TestStandardCard(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="0"
+      direction="column"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      {...rest}
+      {...getOverrideProps(overrides, "TestStandardCard")}
+    >
+      <StandardCard
+        display="flex"
+        gap="0"
+        direction="column"
+        width="320px"
+        justifyContent="center"
+        shrink="0"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        backgroundColor="rgba(255,255,255,1)"
+        {...getOverrideProps(overrides, "TestStandardCard31492681")}
+      ></StandardCard>
+    </Flex>
+  );
+}
