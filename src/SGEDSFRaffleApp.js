bd907194c246c7da4db7ca627812e6ebce992d30
Angelica Lorraine Santiago <angelica.s@cheildubai.local>
2022-06-24 13:28:00 +0400

    Testing minor overrides

--- src/SGEDSFRaffleApp.js
+++ src/SGEDSFRaffleApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './SGEDSFRaffleApp.css';
 
@@ -20,39 +20,81 @@ async function onQuery() {
   console.log(posts);
 }
 
-function Loader() {
-  return <div style={{color: 'black', backgroundColor: 'rgba(0,0,0,0.35)'}} >
-    <img src={logo} className="App-logo" alt="logo" />
-  </div>;
-}
-
 const formOverrides = {
-  FormRegistrationSimple: {width: '100%'},
-  Content: {textAlign: 'start', width: '100vw', maxWidth: '640px', alignSelf: 'center'},
-  BtnRegister: {onClick: async () => {
-    await onQuery().finally(async () => {
-      await new Promise(res => setTimeout(res, 3000));
-      alert("HELLO THERE -- ");
-    });
-  }}
+  FormRegistrationSimple: { width: "100%" },
+  Content: { textAlign: "start", width: "100vw", maxWidth: "640px", alignSelf: "center" },
+  BtnRegister: {
+    onClick: async () => {
+      await onQuery().finally(async () => {
+        alert("HELLO THERE -- ");
+      });
+      // await onQuery().finally(async () => {
+      //   await new Promise(res => setTimeout(res, 3000));
+      //   alert("HELLO THERE -- ");
+      // });
+    }
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: 0
+    }
+
+    // this.myFunction = this.myFunction.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AmplifyUI.FormRegistrationSimple overrides={formOverrides}></AmplifyUI.FormRegistrationSimple>
+      </div>
+    );
+  }
 }
 
-function App() {
+// var isLoading = false;
 
-  useEffect(() => {
-    const subscription = DataStore.observe(Registration).subscribe((msg) => {
-      console.log(msg.model, msg.opType, msg.element);
-    });
-    return () => subscription.unsubscribe();
-  }, []);
+// async function onQuery() {
+//   const posts = await DataStore.query(Registration);
+//   console.log(posts);
+// }
+
+// function Loader() {
+//   return <div style={{color: 'black', backgroundColor: 'rgba(0,0,0,0.35)'}} >
+//     <img src={logo} className="App-logo" alt="logo" />
+//   </div>;
+// }
+
+// const formOverrides = {
+//   FormRegistrationSimple: {width: '100%'},
+//   Content: {textAlign: 'start', width: '100vw', maxWidth: '640px', alignSelf: 'center'},
+//   BtnRegister: {onClick: async () => {
+//     await onQuery().finally(async () => {
+//       await new Promise(res => setTimeout(res, 3000));
+//       alert("HELLO THERE -- ");
+//     });
+//   }}
+// }
+
+// function App() {
+
+//   useEffect(() => {
+//     const subscription = DataStore.observe(Registration).subscribe((msg) => {
+//       console.log(msg.model, msg.opType, msg.element);
+//     });
+//     return () => subscription.unsubscribe();
+//   }, []);
 
 
-  return (
-    <div className="App">
-      {/* <Loader></Loader> */}
-      <AmplifyUI.FormRegistrationSimple overrides={formOverrides}></AmplifyUI.FormRegistrationSimple>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <Loader></Loader>
+//       <AmplifyUI.FormRegistrationSimple overrides={formOverrides}></AmplifyUI.FormRegistrationSimple>
+//     </div>
+//   );
+// }
 
 export default App;
